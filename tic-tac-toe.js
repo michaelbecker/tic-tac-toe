f3e4dbe2f83bec8cd702487718f519ae7573cd69
Michael Becker <michael.f.becker@gmail.com>
2016-11-10 20:46:47 -0500

    Adding more strategy logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,16 @@ const x = 1;
 const o = 2;
 
 
+function getOpponent(player) {
+    if (player == x)
+        return o;
+    else if (player == o)
+        return x;
+    else 
+        throw new Error("Invalid player");
+}
+
+
 function createBoard(rows, cols) {
     var board = new Array(rows);
     for (var i = 0; i < board.length; i++) {
@@ -63,6 +73,9 @@ function printForkingCombinationsToConsole(w) {
 }
 
 
+//-------------------------------------------------------------------
+// Find all possible wins given a board and a player
+//-------------------------------------------------------------------
 function findWins(board, player) {
     
     var winningR = -1;
@@ -221,6 +234,9 @@ function findWins(board, player) {
 }
 
 
+//-------------------------------------------------------------------
+// Find all possible forks given a board and a player
+//-------------------------------------------------------------------
 function findForks(board, player) {
 
     var forkCombinations = [];
@@ -244,12 +260,200 @@ function findForks(board, player) {
 }
 
 
+//-------------------------------------------------------------------
+// Find all possible empty centers of a board
+//-------------------------------------------------------------------
+function findCenters(board) {
+
+    var centers = [];
+    var numCenters = 0;
+
+    // We are going to ignore non-square boards.
+    if (board.length == board[0].length) {
+
+        // Odd sized boards have one square at the center
+        if ((board.length % 2) == 1) {
+            var center = Math.floor(board.length / 2);
+            if (board[center][center] == 0)
+                centers[numCenters++] = [center, center];
+        }
+        // Even sized boards have 4 squares at the center
+        else {
+            var center1 = Math.floor(board.length / 2);
+            var center2 = center1 - 1;
+            if (board[center1][center1] == 0)
+                centers[numCenters++] = [center1, center1];
+            if (board[center1][center2] == 0)
+                centers[numCenters++] = [center1, center2];
+            if (board[center2][center1] == 0)
+                centers[numCenters++] = [center2, center1];
+            if (board[center2][center2] == 0)
+                centers[numCenters++] = [center2, center2];
+        }
+    }
+
+    return centers;
+}
+
 
 //---------------------------------------------------------------
-//  
-//  Reference: https://en.wikipedia.org/wiki/Tic-tac-toe
-//
+//  Find all available corners that are opposite our opponent
 //---------------------------------------------------------------
+function findOppositeCorners(board, player) {
+
+    var oppositeCorners = [];
+    var numOppositeCorners = 0;
+
+    var opponent = getOpponent(player);
+    var corners = [
+                    [0, 0], 
+                    [0, board[0].length - 1],
+                    [board.length - 1, 0], 
+                    [board.length - 1, board[board.length - 1].length - 1]
+                  ];
+    var cornerVals = [
+                        board[0][0],
+                        board[0][board[0].length - 1],
+                        board[board.length - 1][0],
+                        board[board.length - 1][board[board.length - 1].length - 1]
+                     ];
+
+    if ((cornerVals[0] == opponent) && (cornerVals[2] == 0)) 
+        oppositeCorners[numOppositeCorners++] = corners[2];
+    if ((cornerVals[0] == 0) && (cornerVals[2] == opponent)) 
+        oppositeCorners[numOppositeCorners++] = corners[0];
+
+    if ((cornerVals[1] == opponent) && (cornerVals[3] == 0)) 
+        oppositeCorners[numOppositeCorners++] = corners[3];
+    if ((cornerVals[1] == 0) && (cornerVals[3] == opponent)) 
+        oppositeCorners[numOppositeCorners++] = corners[1];
+
+    return oppositeCorners;
+}
+
+
+//---------------------------------------------------------------
+//  Find all empty corners 
+//---------------------------------------------------------------
+function findEmptyCorners(board) {
+
+    var emptyCorners = [];
+    var numEmptyCorners = 0;
+
+    var corners = [
+                    [0, 0], 
+                    [0, board[0].length - 1],
+                    [board.length - 1, 0], 
+                    [board.length - 1, board[board.length - 1].length - 1]
+                  ];
+    var cornerVals = [
+                        board[0][0],
+                        board[0][board[0].length - 1],
+                        board[board.length - 1][0],
+                        board[board.length - 1][board[board.length - 1].length - 1]
+                     ];
+
+    for (var i = 0; i < 4; i++) {
+        if (cornerVals[i] == 0)
+            emptyCorners[numEmptyCorners++] = corners[i];
+    }
+
+    return emptyCorners;
+}
+
+
+//---------------------------------------------------------------
+//  Find empty spots on the sides, ignoring corners
+//---------------------------------------------------------------
+function findEmptySides(board) {
+    
+    var emptySides = [];
+    var numEmptySides = 0;
+
+    // Pin a row down, look in there
+    function emptyInRow(board, row) {
+        for (var i = 1; i < (board[row].length - 1); i++){
+            if (board[row][i] == 0)
+                emptySides[numEmptySides++] = [row, i];
+        }
+    }
+
+    // Pin a column down, look in there
+    function emptyInCol(board, col) {
+        for (var i = 1; i < (board.length - 1); i++){
+            if (board[i][col] == 0)
+                emptySides[numEmptySides++] = [i, col];
+        }
+    }
+
+    emptyInRow(board, 0);
+    emptyInRow(board, board.length - 1);
+    emptyInCol(board, 0);
+    emptyInCol(board, board[0].length - 1);
+
+    return emptySides;
+}
+
+
+//---------------------------------------------------------------
+//  Find the next place to move.
+//  Reference: https://en.wikipedia.org/wiki/Tic-tac-toe
+//---------------------------------------------------------------
+function runStrategy(board, player) {
+
+    // 1) Win
+    var wins = findWins(board, player);
+    if (wins.length >= 1) {
+        return wins[0];
+    }
+
+    // 2) Block
+    var blocks = findWins(board, getOpponent(player));
+    if (blocks.length >= 1) {
+        return blocks[0];
+    }
+
+    // 3) Fork
+    var forks = findForks(board, player);
+    if (forks.length >= 1) {
+        return forks[0];
+    }
+
+    // 4) Block opponent's fork
+    var opponentsForks = findForks(board, getOpponent(player));
+    if (opponentsForks.length >= 1) {
+        return opponentsForks[0];
+    }
+
+    // 5) Center
+    var centers = findCenters(board);
+    if (centers.length >= 1) {
+        return centers[0];
+    }
+
+    // 6) Opposite corner of opponent
+    var oppositeCorners = findOppositeCorners(board, player);
+    if (oppositeCorners.length >= 1) {
+        return oppositeCorners[0];
+    }
+
+    // 7) Empty Corner
+    var emptyCorner = findEmptyCorners(board);
+    if (emptyCorner.length >= 1) {
+        return emptyCorner[0];
+    }
+
+    // 8) Empty Side
+    var emptySides = findEmptySides(board);
+    if (emptySides.length >= 1) {
+        return emptySides[0];
+    }
+
+    // Should never get here.
+    throw new Error("Invalid strategy / board");
+}
+
+
 
 var board = createBoard(numRows, numCols);
 board[0][0] = 0;
@@ -269,9 +473,10 @@ printBoardToConsole(board);
 var wins = findWins(board, o);
 printWiningCombinationsToConsole(wins);
 
+debugger;
 var forks = findForks(board, o);
+debugger;
 printForkingCombinationsToConsole(forks);
 
-
-
+runStrategy(board, o);
 
